19db80d512aadef1fa1a9ba07d7ff6e57327784e
AGBLOGBAOWEL2005 <mawunan14@gmail.com>
2025-12-31 14:39:21 +0000

    Initial commit

---
+++ src/components/connecter/connecter.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-connecter',
+  imports: [],
+  templateUrl: './connecter.html',
+  styleUrl: './connecter.css',
+})
+export class Connecter {
+
+}
