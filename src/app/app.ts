f9f30adb503b4a0741e1f33a9c8a91b8e7d92847
Karimienne <antacisse0924@gmail.com>
2025-12-31 14:36:12 +0100

    quatrieme commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,12 +2,13 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from '../components/header/header';
 import { Hero } from '../components/hero/hero';
+import { Formulaire } from '../components/formulaire/formulaire';
 import { Portfolio } from '../components/portfolio/portfolio';
 import { Footer } from '../components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Header,Hero,Portfolio,Footer],
+  imports: [RouterOutlet,Header,Hero,Formulaire,Portfolio,Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
